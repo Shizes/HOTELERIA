2b1fa8505bfa215711e25dbdcba34d8a9414d084
Shizes <reyshelortiz03@gmail.com>
2024-11-22 23:41:32 -0400

    cambio1

--- app/habitaciones/page.tsx
+++ app/habitaciones/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '@/Components/ui/Navbar'
+import Footer from '@/Components/ui/Footer'
 const habitaciones = () => {
   return (
     <div>
       <Navbar/>
       habitaciones
+      <Footer/>
     </div>
   )
 }
