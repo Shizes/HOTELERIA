66f6a7520c49e117139281a32aeeaa454a5a2ca8
Geraldiene <riomahabramromamateo@gmail.com>
2024-11-24 19:03:43 -0400

    v5 api

--- lib/Api.ts
+++ lib/Api.ts
@@ -1,26 +1,32 @@
 export async function getData() {
+    try {
     //principal
     const resServicios = await fetch('https://673e55760118dbfe860b0087.mockapi.io/api/v1/servicios');
     //habitaciones
-    const resHabitaciones = await fetch('');
+    //const resHabitaciones = await fetch('');
     //eventos y salones
     const resEventos = await fetch('https://673e808e0118dbfe860b76dd.mockapi.io/eventos');
     const resSalones = await fetch('https://673e808e0118dbfe860b76dd.mockapi.io/armados');
 
-    const [servicios, habitaciones, eventos, salones] = await Promise.all([
+    if (!resServicios.ok ||/* !resHabitacione.ok ||*/ !resEventos.ok || !resSalones.ok) {
+        throw new Error('Error al obtener uno o más recursos');
+      }
+
+    const [servicios, /*habitaciones,*/ eventos, salones] = await Promise.all([
         resServicios.json(),
-        resHabitaciones.json(),
+        //resHabitaciones.json(),
         resEventos.json(),
         resSalones.json(),
     ]);
 
     return {
-        props: {
-            servicios,
-            habitaciones,
-            eventos,
-            salones,
-        },
-    };
-    
-}
+        servicios,
+        //habitaciones,
+        eventos,
+        salones,
+      };
+    } catch (error) {
+      console.error('Error en getData:', error);
+      throw new Error('No se pudieron cargar los datos');
+    }
+  }
