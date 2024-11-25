326093e84d890ff1f905c01ac04aff772e4f5618
Geraldiene <riomahabramromamateo@gmail.com>
2024-11-25 16:55:37 -0400

    correccion 2 Api

--- app/armados/page.tsx
+++ app/armados/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Navbar from '@/Components/ui/Navbar'
+import { getApis } from '@/lib/getApis'
+
 const armados = () => {
   return (
     <div>
@@ -7,6 +9,21 @@ const armados = () => {
       armados
       </div>
   )
+}
+
+export async function getServerSideProps(){
+  try {
+    const salones = await getApis()
+    return{
+      props:{
+        salones
+    }
+  };
+  }catch(error){
+    return{
+      props: { articles: [] },
+    };
+  }
 }
 
 export default armados
--- lib/getApis.ts
+++ lib/getApis.ts
@@ -1,9 +1,27 @@
 export async function getApis() {
-    const res = await fetch('https://api.hotel.com/api/v1/hotel/services');
+    try {
+        const resSalones = await fetch('https://api.hotel.com/api/v1/hotel/services/salones'); // Cambia la URL según sea necesario
+        const resEventos = await fetch('https://api.hotel.com/api/v1/hotel/services/eventos'); // Cambia la URL según sea necesario
 
-    if (!res.ok) {
-        throw new Error('Failed to fetch API');
+        // Verifica si alguna de las respuestas no es correcta
+        if (!resSalones.ok) {
+            throw new Error('Error al recuperar los datos de salones');
+        }
+        if (!resEventos.ok) {
+            throw new Error('Error al recuperar los datos de eventos');
+        }
+
+        // Convierte las respuestas a JSON
+        const dataSalones = await resSalones.json();
+        const dataEventos = await resEventos.json();
+
+        // Devuelve ambos conjuntos de datos
+        return {
+            salones: dataSalones,
+            eventos: dataEventos,
+        };
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error en la recuperación de datos: ');
     }
-    const data = await res.json();
-    return data;
 }
