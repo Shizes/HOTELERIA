e29e2ec5d2a9ee2386086809839e0da6cb7d19ad
Geraldiene <riomahabramromamateo@gmail.com>
2024-11-26 11:58:44 -0400

    part api

--- Components/CardEvento.tsx
+++ Components/CardEvento.tsx
@@ -1,19 +1,10 @@
+import "./CardEvento.css";
+
 import React from 'react'
-import 'CardEvento.css';
 
 const CardEvento = () => {
   return (
-    <section className="eventos">
-      <div className="eventos-card">
-        <img src="" alt="" />
-        <div>
-        <h3></h3>
-        <p></p>
-        <button>Reservar</button>
-        <button></button>
-        </div>
-      </div>
-    </section>
+    <div>CardEvento</div>
   )
 }
 
--- Components/Eventos.tsx
+++ Components/Eventos.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import CardEvento from './CardEvento';
+'use server';
+import { getEventos } from "../lib/geEventos";
+import CardEvento from "./CardEvento";
 
-
-const Eventos = () => {
-  return (
-    <div>Eventos</div>
-  )
+interface CardEventsType {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
 }
 
-export default Eventos
+export default async function ApiEvents() {
+  // Obtener datos del servidor
+  const cardEvents: CardEventsType[] = await getEventos(
+    "https://673e808e0118dbfe860b76dd.mockapi.io/eventos"
+  );
 
-//llamado a la api
+  const titleStyle = {
+    fontSize: "24px",
+    fontWeight: "bold",
+    color: "#A91F8C",
+    textAlign: "center" as const,
+    marginBottom: "20px",
+    fontFamily: "Dancing Script",
+  };
+
+  return (
+    <div>
+      <h2 style={titleStyle}>Tipo de Eventos</h2>
+      {cardEvents.map((card) => (
+        <CardEvento key={card.id} cardEvent={card} />
+      ))}
+    </div>
+  );
+}
