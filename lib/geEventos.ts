a051403e3c1d1cc778a0041a126dda38cbd77888
Geraldiene <riomahabramromamateo@gmail.com>
2024-11-26 11:06:12 -0400

    funcion api

---
+++ lib/geEventos.ts
@@ -0,0 +1,8 @@
+export async function getEventos(url: string) {
+    const response = await fetch(url, { cache: 'no-store' }); // cache: 'no-store' asegura datos frescos
+    if (!response.ok) {
+      throw new Error(`Error al obtener los datos: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+}
