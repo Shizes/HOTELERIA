85f57ca6033b302e5b74f30a5f49dd1ff6ec86af
Geraldiene <riomahabramromamateo@gmail.com>
2024-11-26 12:18:04 -0400

    card

--- Components/CardSalon.tsx
+++ Components/CardSalon.tsx
@@ -1,8 +1,28 @@
+import salones from '@/app/salones/page';
 import React from 'react'
+
+interface SalonProps {
+  Salon: {
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+  };
+}
 
 const CardSalon = () => {
   return (
-    <div>CardSalon</div>
+    <section>
+      <div className='mosaico'>
+        <div className='card'>
+          <img src={salon.image} alt={salon.name} />
+          <div className='descript'>
+            <h3>{salon.name}</h3> 
+            <button><img src="/icon/botnondesplegable.svg" alt="" /></button>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
