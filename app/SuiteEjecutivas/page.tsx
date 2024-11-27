c180ae31b86a255d5ca89f0a68070fe960d326f8
Shizes <reyshelortiz03@gmail.com>
2024-11-27 10:23:44 -0400

    intento uno

--- app/SuiteEjecutivas/page.tsx
+++ app/SuiteEjecutivas/page.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import Navbar from '@/Components/ui/Navbar'
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import Navbar from "@/Components/ui/Navbar";
+import Footer from "@/Components/ui/Footer";
+import PersonSelector from "@/Components/PersonSelector";
+import DateSelector from "@/Components/DateSelector";
+import RoomSection from "@/Components/RoomSection";
+import ExtraButton from "@/Components/ExtraButton";
+import "./page.css";
 const suitesejecutivas = () => {
   return (
     <div>
       <Navbar/>
-      suitesejecutivas</div>
+      suitesejecutivas
+      <Footer/>
+      </div>
   )
 }
 
