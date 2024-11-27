7f5631e57e8d8e3ded88c9ad079a3da0bad82b7e
Shizes <reyshelortiz03@gmail.com>
2024-11-27 15:50:03 -0400

    arreglos2

---
+++ app/iniciosesion/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Portada from '@/components/portada';
+
+const InicioSesion = () => {
+  return (
+    <div>
+      <Portada />
+      <main>
+      </main>
+    </div>
+  );
+};
+
+export default InicioSesion;
