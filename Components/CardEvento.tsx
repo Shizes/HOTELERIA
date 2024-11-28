a420a061c5b24559e16351a2e5886de2364729d2
Geraldiene <riomahabramromamateo@gmail.com>
2024-11-27 21:22:53 -0400

    v9

--- Components/CardEvento.tsx
+++ Components/CardEvento.tsx
@@ -15,25 +15,16 @@ interface CardEventProps {
 const CardEvento = ({ cardEvent }: CardEventProps) => {
   return (
     <section className="eventos-card">
-      <div className="image-container">
-        <img src={cardEvent.avatar} alt="" />
-        <button className="cart-button">
-          <Image src="/img/compra.png" height={20} width={20} alt="" />
-    </button>
-      </div>
+      <img src={cardEvent.avatar} alt="" />
       <div className="text">
         <div className="title">
           <h3>{cardEvent.name}</h3>
         </div>
         <div className="botones">
           <button className="det">
-            <Image src="/img/plus.png" height={20} width={20} alt="" />{" "}
-            <p>DESCRIPCIÓN</p>
+            <Image src="/img/plus.png" height={20} width={20} alt="" /> <p>DESCRIPCIÓN</p>
           </button>
-          <a href="../armados/page.tsx">
-            {" "}
-            <button className="reser">Reservar</button>
-          </a>
+          <a href="../armados/page.tsx"> <button className="reser">Reservar</button></a>
         </div>
         <div className="deplegable">
           <p className="description">{cardEvent.description}</p>
