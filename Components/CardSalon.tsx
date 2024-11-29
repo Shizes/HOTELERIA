cefaa07bc5a236d1988c8bd24ee77d311435824c
KaitoEG <eaghtspr@gmail.com>
2024-11-29 02:15:29 -0400

    cardSalones

--- Components/CardSalon.tsx
+++ Components/CardSalon.tsx
@@ -17,16 +17,10 @@ const CardSalon = ({ salon }: SalonProps) => {
     <div className="card">
       <div className="image-container">
         <img src={salon.image} alt={salon.name} />
-        <button className="cart-button">
-          <Image src="/img/compra.png" width={10} height={10} alt="" />
-        </button>
       </div>
 
       <div className="descript">
         <p>{salon.name}</p>
-        <button>
-          <Image src="/img/desplegable.png" width={10} height={10} alt="" />
-        </button>
       </div>
       <div className="descripcion">
         <img src={salon.description} alt={salon.name} />
