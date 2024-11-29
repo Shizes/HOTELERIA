26ef6206b694c6ca062ee83d30ca238ab58e0b37
KaitoEG <eaghtspr@gmail.com>
2024-11-29 01:47:57 -0400

    habitaciones a un paso de pago

--- app/Pagos/page.tsx
+++ app/Pagos/page.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+"use client";
 
-const pagos
- = () => {
+import React from "react";
+import { useSearchParams } from "next/navigation";
+
+const Pagos = () => {
+  const searchParams = useSearchParams();
+  const price = searchParams.get("price");
+
+  if (!price) {
+    return <p>No se seleccionó ninguna habitación.</p>;
+  }
+
   return (
-    <div>pagos
-        
+    <div>
+      <h1>Página de Pago</h1>
+      <p>El monto a pagar por esta habitación es:</p>
+      <h2>${price}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default pagos
+export default Pagos;
